3b4d75e523037d1d962e379b3579f1d87e507d22
Miguel-Angel-Morales <angelmoralesm10@gmail.com>
2023-07-14 11:59:32 -0500

    added CRUD

---
+++ models/location.py
@@ -0,0 +1,8 @@
+class Location():
+
+    def __init__(self, id, name, address):
+        self.id = id
+        self.name = name
+        self.address = address
+
+new_location = Location(3, "Rocky", "121 St Baltimore Maryland")
